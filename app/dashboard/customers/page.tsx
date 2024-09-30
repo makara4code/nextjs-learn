6ab00c102d2dad4bf127757de74a179a44c243d2
Makara Nuol <makara.nuol@wingmoney.com>
2024-09-30 10:21:11 +0700

    add hono epi endpoint

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -5,5 +5,12 @@ export const metadata: Metadata = {
 };
 
 export default function Customers() {
+  const fetchHello = async () => {
+    const res = await fetch("/api/hello");
+    const json = await res.json();
+    console.log({ json });
+  }
+
+  fetchHello();
   return <h1>Customers</h1>;
 }
